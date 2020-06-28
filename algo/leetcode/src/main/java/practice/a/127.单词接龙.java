b65331127b30da69a65a993cfbdc28d5d24941e0
shixy96 <shixy@feifanuniv.com>
2020-06-28 22:16:20 +0800

    modify directory

--- algo/leetcode/src/main/java/practice/a/127.单词接龙.java
+++ algo/leetcode/src/main/java/practice/a/127.单词接龙.java
@@ -64,63 +64,8 @@ import java.util.*;
  */
 
 // @lc code=start
-class Solution127 {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+class Solutiona127 {
 
-        // Since all words are of same length.
-        final int wordLength = beginWord.length();
-
-        // Dictionary to hold combination of words that can be formed,
-        // from any given word. By changing one letter at a time.
-        Map<String, List<String>> allComboDict = new HashMap<>();
-
-        wordList.forEach(
-                word -> {
-                    for (int i = 0; i < wordLength; i++) {
-                        // Key is the generic word
-                        // Value is a list of words which have the same intermediate generic word.
-                        String newWord = word.substring(0, i) + '*' + word.substring(i + 1, wordLength);
-                        List<String> transformations = allComboDict.getOrDefault(newWord, new ArrayList<>());
-                        transformations.add(word);
-                        allComboDict.put(newWord, transformations);
-                    }
-                });
-
-        // Queue for BFS
-        Queue<Pair<String, Integer>> Q = new LinkedList<>();
-        Q.add(new Pair(beginWord, 1));
-
-        // Visited to make sure we don't repeat processing same word.
-        Map<String, Boolean> visited = new HashMap<>();
-        visited.put(beginWord, true);
-
-        while (!Q.isEmpty()) {
-            Pair<String, Integer> node = Q.remove();
-            String word = node.getKey();
-            int level = node.getValue();
-            for (int i = 0; i < wordLength; i++) {
-
-                // Intermediate words for current word
-                String newWord = word.substring(0, i) + '*' + word.substring(i + 1, wordLength);
-
-                // Next states are all the words which share the same intermediate state.
-                for (String adjacentWord : allComboDict.getOrDefault(newWord, new ArrayList<>())) {
-                    // If at any point if we find what we are looking for
-                    // i.e. the end word - we can return with the answer.
-                    if (adjacentWord.equals(endWord)) {
-                        return level + 1;
-                    }
-                    // Otherwise, add it to the BFS Queue. Also mark it visited
-                    if (!visited.containsKey(adjacentWord)) {
-                        visited.put(adjacentWord, true);
-                        Q.add(new Pair(adjacentWord, level + 1));
-                    }
-                }
-            }
-        }
-
-        return 0;
-    }
 }
 
 // @lc code=end
